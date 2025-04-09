f0c9db2dd0e7d60a4e8e463fb5da2cb748920c9d
Serge Gorbachev <sng78@list.ru>
2025-04-09 12:03:27 +0300

    add dto

---
+++ src/main/java/io/github/sng78/to/MeasurementTo.java
@@ -0,0 +1,42 @@
+package io.github.sng78.to;
+
+import io.github.sng78.model.Sensor;
+
+public class MeasurementTo {
+
+    private double value;
+
+    private boolean isRaining;
+
+    private Sensor sensor;
+
+    public MeasurementTo(double value, boolean isRaining, Sensor sensor) {
+        this.value = value;
+        this.isRaining = isRaining;
+        this.sensor = sensor;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return isRaining;
+    }
+
+    public void setRaining(boolean raining) {
+        isRaining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
---
+++ src/main/java/io/github/sng78/to/SensorTo.java
@@ -0,0 +1,18 @@
+package io.github.sng78.to;
+
+public class SensorTo {
+
+    private String name;
+
+    public SensorTo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

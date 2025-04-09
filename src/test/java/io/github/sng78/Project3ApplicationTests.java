1ad2579a9d7cbf8226ce07320ec33303e142e06f
Serge Gorbachev <sng78@list.ru>
2025-04-09 09:47:59 +0300

    first commit

---
+++ src/test/java/io/github/sng78/Project3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sng78;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
